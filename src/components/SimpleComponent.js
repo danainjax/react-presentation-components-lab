bada9ed784421274fee6b2b29aebea8264aedcb9
danainjax <danainjax@gmail.com>
2021-10-31 14:21:48 -0400

    commit tree 2e54fffeedfd5b36793bcad137f37b9869bc7cfd

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,18 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+export default class SimpleComponent extends Component {
+  constructor() {
+    super();
+    this.state = { mood: "happy" };
+  }
+
+  toggleState = () => {
+    const changeMood = this.state.mood === "happy" ? "sad" : "happy";
+    this.setState({ mood: changeMood });
+  };
+
+  render() {
+    return <div onClick={this.toggleState}>{this.state.mood}</div>;
+  }
+}
